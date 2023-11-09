f76b4f5e594d8390dda9464027c71b97961b7270
Dtine <diofiloi22@gmail.com>
2023-11-09 22:05:52 +0100

    dummy

--- free-code/Problem-1.cpp
+++ free-code/Problem-1.cpp
@@ -9,17 +9,67 @@ using namespace std;
 
 int main()
 {
-    unordered_map<char,bool> sheet;
-    string s = "hellodworld";
+    string s = "hellheodwrorld";
+    string alph = "";
     string ans = "";
-    for(int i = 0;i < s.size();i++)
+
+    //составление алфавита из строки s;
+
+    for(int i = 0;i<s.size();i++)
     {
-        if(!sheet[s[i]])
+        if(find(alph.begin(),alph.end(),s[i])==alph.end())
         {
-            ans = s.substr(0,i+1);
-            sheet[s[i]] = 1;
+            alph+=s[i];
         }
     }
-    cout<<ans;
+
+    //поиск ответа
+
+    size_t sz=INT_MAX;
+    int first = 0;
+    int second = 0;
+    int i = 0;
+    int j = 0;
+    while(j<s.size())
+    {
+        string temp = s.substr(i,j-i+1);
+        if(j-i+1 > 1 && temp[0] == temp[temp.size()-1]||temp[0]==temp[1])
+        {
+            i++;
+            continue;
+        }
+        if(temp.size()<alph.size())
+        {
+            j++;
+            continue;
+        }
+        int ii = 0;
+        while(ii<alph.size())
+        {
+            bool ok = false;
+            for(int jj = 0;jj<temp.size();jj++)
+            {
+                if(alph[ii]==temp[jj])
+                {
+                    ok=true;
+                    break;
+                }
+            }
+            if(!ok)break;
+            ii++;
+        }
+        if(ii==alph.size())
+        {
+            if(sz>j-i+1)
+            {
+                first=i;
+                second=j;
+                sz=j-i+1;
+            }
+        }
+        j++;
+    }
+    
+    cout<<first<<" "<<second<<" "<<s.substr(first,second-first+1);
     return 0;
 }
