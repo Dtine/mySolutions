1034404711e763788806c8c6c8d4a804b1b4755b
Dtine <diofiloi22@gmail.com>
2023-11-22 17:24:25 +0100

    Пофиксил баг, с индексацией обьектов в списке. - При вызове метода .get(...) возникал случай,   когда обьекты удалялись.
    Отказался от перегрузок операторв индексации и присваивания
    
    Добавил методы .get(...) и .set(... , ...)

--- free-code/Problem-3.cpp
+++ free-code/Problem-3.cpp
@@ -9,29 +9,23 @@
 using namespace std;
 
 
-template<typename Key,typename Name>
+template<typename Key = int,typename Name = int>
 class S
-{
+{   
+    public:
     pair<Key,Name> values;
     S* next;
-
     S()
     {
         values.first = NULL;
         values.second = NULL;
         next = nullptr;
     }
-    
     S(Key key,Name name)
     {
         values.first = key;
         values.second = name;
         next = nullptr;
-    }
-    S(pair<Key,Name> values, S* next)
-    {
-        this->values=values;
-        this->next=next;
     }
 };
 
@@ -46,12 +40,13 @@ class hash_map
     
     void increase_size()
     {
-        vector<S<Key,Name>*> temp(bucket_size*2,nullptr);
         bucket_size*=2;
+        vector<S<Key,Name>*> temp(bucket_size,nullptr);
 
         for(int i = 0;i<bucket_size/2;i++)
         {
-            connector(buckets[i],temp);    
+            if(buckets[i]!=nullptr)
+                connector(buckets[i],temp);    
         }
         
         buckets=temp;
@@ -61,7 +56,7 @@ class hash_map
     {
         while(List!=nullptr)
         {
-            size_t new_index = hash<Key>{}(List->first.first);
+            size_t new_index = hash<Key>{}(List->values.first);
             new_index%=bucket_size;
             if(temp[new_index] != nullptr)
             {
@@ -89,19 +84,84 @@ class hash_map
         bucket_size = 5;
         num_of_elements = 0;
     } 
-    /* 
-    Name& operator[](const Key key)
-    {  
-        
-       ...    still in progress
-
+    void set(Key key,Name name)
+    {
+        if(bucket_size == num_of_elements)
+        {
+            increase_size();
+        }
+        size_t index = hash<Key>{}(key);
+        index%=bucket_size;
+        if(buckets[index] == nullptr)
+        {
+            buckets[index] = new S(key,name);
+            num_of_elements++;
+            return;
+        }
+        S<Key,Name>* head = buckets[index];
+        while(buckets[index]->next != nullptr)
+        {
+            if(buckets[index]->values.first == key)
+            {
+                break;
+            }
+            buckets[index] = buckets[index]->next;
+        }
+        if(buckets[index]->values.first != key)
+        {
+            num_of_elements++;
+            buckets[index]->next = new S(key,name);
+        }
+        else
+        {
+            buckets[index]->values.second = name;
+        }
+        buckets[index] = head;
+        return;
     }
-    */ 
+    Name get(Key key)
+    {
+        size_t index = hash<Key>{}(key);
+        index%=bucket_size;
+        S<Key,Name>* head = buckets[index];
+        S<Key,Name>* curr = buckets[index];
+        while(curr->next != nullptr)
+        {
+            if(curr->values.first == key)
+            {
+                break;
+            }
+            curr=curr->next;
+        }
+        if(curr->values.first != key)
+        {
+            curr->next = new S(key,0);
+            curr=curr->next;
+            num_of_elements++;
+        }
+        return curr->values.second;
+    }
+    
 };
 
 int main()
 {
     hash_map<int,int> mp;
+    mp.set(5,6);
+    mp.set(10,11);
+    mp.set(15,16);
+    cout<<mp.get(5)<<endl;
+    cout<<mp.get(10)<<endl;
+    cout<<mp.get(15)<<endl;
+    cout<<mp.get(20)<<endl;
+    mp.set(20,21);
+    cout<<mp.get(20)<<endl;
+    mp.set(26,27);
+    cout<<mp.get(5)<<endl;
+    cout<<mp.get(10)<<endl;
+    cout<<mp.get(15)<<endl;
+    cout<<mp.get(20)<<endl;
+    cout<<mp.get(26)<<endl;
 
 
 
